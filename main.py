12c0339ccb00d0e44964eb89402419e60e27180f
jamylj <johnsonjamyl@gmail.com>
2022-01-19 10:38:25 -0700

    added doc strings for functions

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 def multiply(num1, num2):
+    """ multiplies two numbers
+
+    Args:
+        num1 ([type]): [description]
+        num2 ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
     return num1*num2
 
 def add(num1, num2):
+    """ Adds two numbers
+
+    Args:
+        num1 ([type]): [description]
+        num2 ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+
     return num1+num2 
